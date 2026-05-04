6ce8def5122b6e41bbd26ec5d8a754a432f4780f
Vimal <Vimal115077286@gmail.com>
2026-05-04 22:11:43 +0530

    storage added to save selected places

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,13 @@ import logoImg from './assets/logo.png';
 
 import { sortPlacesByDistance } from "./loc.js";
 
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || []
+const selectedPlaces = storedIds.map((id) => AVAILABLE_PLACES.find((place) => place.id === id ))
+
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(selectedPlaces);
   const [available_places, setAvaliablePlaces] = useState([])
 
   useEffect(() => {
@@ -20,7 +23,7 @@ function App() {
 
       setAvaliablePlaces(sortedPlaces)
     })
-  },[])
+  }, [])
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -39,6 +42,11 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || []
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIds]))
+    }
   }
 
   function handleRemovePlace() {
@@ -46,6 +54,10 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || []
+    localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)))
+
   }
 
   return (
